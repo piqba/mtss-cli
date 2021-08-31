9a5d73332cbf01d61ce9712ac794bf7793669a81
WilsonBolanosSoto <wilson.bolanossoto@gmail.com>
2021-08-31 12:35:18 -0400

    refactor: add redis client for job ingestions

---
+++ pkg/redisdb/redis.go
@@ -0,0 +1,33 @@
+package redisdb
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/piqba/mtss-go/pkg/errors"
+	"github.com/piqba/mtss-go/pkg/logger"
+)
+
+var (
+	ErrRedisDbCheckConn = errors.NewError("Redis: Fail to check connection")
+)
+
+func GetRedisDbClient() *redis.Client {
+
+	clientInstance := redis.NewClient(&redis.Options{
+		Addr:         os.Getenv("REDIS_URI"),  // use default Addr
+		Password:     os.Getenv("REDIS_PASS"), // no password set
+		DB:           0,
+		DialTimeout:  60 * time.Second,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+	})
+
+	_, err := clientInstance.Ping(context.TODO()).Result()
+	if err != nil {
+		logger.LogError(ErrRedisDbCheckConn.Error())
+	}
+	return clientInstance
+}
