6b21b4991ca4c5630dc23e3129e54f803a3d6655
WilsonBolanosSoto <wilson.bolanossoto@gmail.com>
2021-08-31 12:42:32 -0400

    feat: add root cmd for mtss cli

---
+++ cmd/cli/root.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	flagLimit  = "limit"
+	flagEngine = "engine"
+	REDIS      = "redis"
+	MONGODB    = "mongodb"
+	ALL        = "all"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "mtssctl",
+	Short: "A brief description of your application",
+	Long:  `this is Mtss ctl`,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		//TODO change this with tomatto-logger
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
+func init() {
+
+}
